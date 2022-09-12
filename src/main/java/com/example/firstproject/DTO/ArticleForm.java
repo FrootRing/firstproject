6bd6f7a27f9ba627b994da5e493c835babe0e19a
junghwan <junghwan2401@gmail.com>
2022-09-12 17:47:27 +0900

    22.09.12.1743

--- src/main/java/com/example/firstproject/DTO/ArticleForm.java
+++ src/main/java/com/example/firstproject/DTO/ArticleForm.java
@@ -1,20 +1,18 @@
 package com.example.firstproject.DTO;
 
+import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private String title;
     private String contents;
 
-    public ArticleForm(String title, String contents){
-        this.title = title;
-        this.contents = contents;
-    }
 
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", contents='" + contents + '\'' +
-                '}';
+    public Article toEntity() {
+        return new Article(null, title, contents);
     }
 }
--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,21 +1,37 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.DTO.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
-    public String newArticleForm() {
-        return "articles/view";
-    }
+    public String newArticleForm() {return "articles/view";}
 
     @PostMapping("/articles/create")
     public String createArticles(ArticleForm form){
-        System.out.println(form.toString());
+        log.info(form.toString());
+        //System.out.println(form.toString());
+
+        //1. Dto를 변환한다 Entity!
+        Article article = form.toEntity();
+        log.info(article.toString());
+        //System.out.println(article.toString());
+
+        //2. Repository에게  Entity를 DB에 저장하게 함!
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         return "";
     }
 }
