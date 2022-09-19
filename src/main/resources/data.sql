6e51cd1380bdde692438db6a043aa21abee61b7c
junghwan <junghwan2401@gmail.com>
2022-09-19 14:12:54 +0900

    22.0919.1412

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, contents) VALUES(1, 'aaa', '111');
+INSERT INTO article(id, title, contents) VALUES(2, 'bbb', '222');
+INSERT INTO article(id, title, contents) VALUES(3, 'ccc', '333');
